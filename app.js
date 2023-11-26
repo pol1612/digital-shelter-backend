29502bf05bb13503171273c98435837c4c103085
Pol Sañé Jové <pol.sane@uvic.cat>
2023-11-26 19:24:37 +0000

    imported endpoint into the app.js file

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+
 const express = require("express")
+
+const dogsRoutes = require('./routes/dog.routes')
 
 const app = express()
 
@@ -7,5 +10,5 @@ const app = express()
 app.set("port",3050)
 
 app.use(express.json())
-
+app.use(dogsRoutes.dogs)
 module.exports = app;
--- routes/dog.routes.js
+++ routes/dog.routes.js
@@ -1,12 +1,13 @@
 const Router = require('express')
 const connection = require('../database/db')
 const dogFakeData = require("../fake-data/dog.data")
+
 const router = Router()
 
-const dispositivos = router.get("/dog",)
+const dogs = router.get("/dog",)
 
 //const dispositivo = router.get("/dispositivo/:id", moviesControllers.getDispositivo)
 
 module.exports = {
-    dispositivo,dispositivos
+    dogs
 }
